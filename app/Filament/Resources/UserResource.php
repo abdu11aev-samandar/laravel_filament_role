3c71ca52f68fc91269287e34acd75e3e2c1ae0c1
samandar <xkas2001@gmail.com>
2023-08-10 15:47:57 +0500

    Made role

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -45,6 +45,14 @@ class UserResource extends Resource
                         ->dehydrated(fn($state) => filled($state))
                         ->required(fn(Page $page) => ($page instanceof Pages\CreateUser))
                         ->maxLength(255),
+                    Forms\Components\Select::make('roles')
+                        ->multiple()
+                        ->relationship('roles', 'name')
+                        ->preload(),
+                    Forms\Components\Select::make('permissions')
+                        ->multiple()
+                        ->relationship('permissions', 'name')
+                        ->preload()
                 ])->columns(2)
             ]);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return $this->hasRole('Admin');
+        return $this->hasRole('Admin', 'Writer');
     }
 }
