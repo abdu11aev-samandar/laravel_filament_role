f51a3202626a59de6b74359d61f54dc258be0ae3
samandar <xkas2001@gmail.com>
2023-08-10 13:42:03 +0500

    Installed Spatie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +18,17 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user1 = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'TestUser',
+            'email' => 'test@admin.com',
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+        $user1->assignRole($role);
     }
 }
